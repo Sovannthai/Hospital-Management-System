a96008ddaf63a6e07c9f95360edb995a4ad92c22
Thai Sros <srosthai00@gmail.com>
2024-04-04 20:32:42 +0700

    New Project

---
+++ resources/views/backends/layout/sidebar.blade.php
@@ -0,0 +1,62 @@
+<aside class="main-sidebar elevation-4 sidebar-light-info" style="width: 280px">
+    <!-- Brand Logo -->
+    {{-- <a href="{{ route('dashboard') }}" class="brand-link">
+        <img src="{{ asset('uploads/business_logo/' . session('business_logo')) }}" alt="AdminLTE Logo" class="brand-image"
+            style="width: 100%;
+      object-fit: contain;margin-left: 0; height: 200px;max-height: 100px;">
+    </a> --}}
+    <a href="" class="brand-link">
+        <img src="{{ asset('uploads/all_photo/logo.png') }}" alt="AdminLTE Logo" class="brand-image"
+            style="width: 100%;
+      object-fit: contain;margin-left: 0; height: 200px;max-height: 100px;">
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                {{-- <img src="{{ Session::get('current_user')->profiles }}" class="img-circle elevation-2"
+                alt="User Image"> --}}
+            </div>
+            <div class="info">
+                {{-- <a href="#" class="d-block">{{ Session::get('current_user')->name }}</a> --}}
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-3 width-nav">
+            <ul class="nav nav-pills nav-sidebar flex-column" id="sidebar-menu" data-widget="treeview" role="menu"
+                data-accordion="true">
+                <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                <li class="nav-item" id="menu_dashboard">
+                    {{-- menu-open --}}
+                    <a href="#" class="nav-link custom-ml" id="menu_dashboard_bg">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item" id="menu_employee_mg">
+                    {{-- menu-open --}}
+                    <a href="#" class="nav-link custom-ml" id="menu_employee_bg">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>User Management
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview" id="collapse_employee">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link custom-ml" id="menu_employess">
+                                <i class="fa-solid fa-plus nav-icon"></i>
+                                <p>User</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/backends/master.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @include('backends.layout.head')
+{{-- <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed"> --}}
+<body class="layout-fixed layout-navbar-fixed text-sm">
+
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+    @include('backends.layout.header')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('backends.layout.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <br>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      @yield('contents')
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer text-center">
+    <div class="float-right d-none d-sm-block">
+      {{-- <b>Version</b> 3.2.0 --}}
+    </div>
+
+    <strong>©Copyright-DevTeam || @ 2024</strong>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark" style="width: 300px;">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+@include('backends.layout.script')
+@yield('backends.script')
+
+<!-- Page level plugins -->
+<script src="{{asset("plugins/chart.js/Chart.min.js")}}"></script>
+<!-- Page level custom scripts -->
+<script src="{{asset("js/charts/chart-area-demo.js")}}"></script>
+<script src="{{asset("js/charts/chart-pie-demo.js")}}"></scrip>
+
+<script>
+    $('#myModal').on('show.bs.modal', function(e) {
+            $(this).find('.btn-ok').attr('action', $(e.relatedTarget).data('href'));
+        });
+    $('#rejectModal').on('show.bs.modal', function(e) {
+        $(this).find('.btn-ok').attr('action', $(e.relatedTarget).data('href'));
+    });
+    $('#approveModal').on('show.bs.modal', function(e) {
+        $(this).find('.btn-ok').attr('action', $(e.relatedTarget).data('href'));
+    });
+</script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+</html>
